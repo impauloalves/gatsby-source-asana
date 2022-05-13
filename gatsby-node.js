0f68a8231764a5fbe4e253fcf41fc9a1d8cee635
Paulo Alves <impauloalves@gmail.com>
2022-05-13 17:24:11 +0100

    Start using axios instead node-fetch

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,52 @@
-const fetch = require("node-fetch").default;
+const axios = require("axios");
 
 async function getTasks(projectId, fields, token) {
   const tasks = [];
   let offset = "";
-  do {
-    let url = `https://app.asana.com/api/1.0/projects/${projectId}/tasks?limit=100`;
-    if (fields) url += `&opt_fields=${fields.join(',')}`
-    if (offset) url += `&offset=${offset}`;
-    const { data, next_page } = await (
-      await fetch(url, {
+  try {
+    do {
+      let url = `https://app.asana.com/api/1.0/projects/${projectId}/tasks?limit=100`;
+      if (fields) url += `&opt_fields=${fields.join(",")}`;
+      if (offset) url += `&offset=${offset}`;
+      const { data: response } = await axios(url, {
         method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
           Accept: "application/json",
         },
-      })
-    ).json();
-    tasks.push(...data);
-    offset = next_page && next_page.offset || ""
-  } while (offset);
+      });
+      const { data, next_page } = response;
+      tasks.push(...data);
+      offset = (next_page && next_page.offset) || "";
+    } while (offset);
+  } catch (error) {
+    throw new Error(JSON.stringify(error.response.data));
+  }
   return tasks;
 }
 
-exports.sourceNodes = async ({
-  actions,
-  createContentDigest,
-  createNodeId,
-}, {apiToken, projectId, fields}) => {
+exports.sourceNodes = async (
+  { actions, createContentDigest, createNodeId },
+  { apiToken, projectId, fields }
+) => {
   // Arbitrary node type constant
   const ITEM_TYPE = "Task";
 
-  // Get tasks
-  const tasks = await getTasks(projectId, fields, apiToken);
-  // Convert raw book results to nodes
-  for (task of tasks) {
-    actions.createNode({
-      id: createNodeId(`${ITEM_TYPE}-${task.gid}`),
-      ...task,
-      internal: {
-        type: ITEM_TYPE,
-        contentDigest: createContentDigest(task),
-      },
-    });
+  try {
+    // Get tasks
+    const tasks = await getTasks(projectId, fields, apiToken);
+    // Convert raw book results to nodes
+    for (task of tasks) {
+      actions.createNode({
+        id: createNodeId(`${ITEM_TYPE}-${task.gid}`),
+        ...task,
+        internal: {
+          type: ITEM_TYPE,
+          contentDigest: createContentDigest(task),
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
